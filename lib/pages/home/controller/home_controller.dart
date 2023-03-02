65c2273d9d02a936ee3a5938f9c49996acf6fab9
ParrottKim <parrottkim21@gmail.com>
2023-03-02 10:47:15 +0900

    changing application layout

---
+++ lib/pages/home/controller/home_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// final recentCommentProvider = StateNotifierProvider<RecentCommentNotifier, >((ref) {
+//   return 
+// });
+
+// class RecentCommentNotifier extends StateNotifier<> {
+//   RecentCommentNotifier(): super();
+  
+// }
